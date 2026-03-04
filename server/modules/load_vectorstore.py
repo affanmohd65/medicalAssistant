3bc20def7b7d4f12504c9f35a47575f7b17bfec4
Mohammad Affan <affanmohd65@gmail.com>
2026-03-04 14:16:30 +0530

    Fix langchain text splitter import

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders.pdf import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
